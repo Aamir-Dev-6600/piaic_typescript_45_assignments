8f75e719124ba0566a3d5ef988c853d955c67b58
Aamir Khan Maarofi <kaamir6600@gmail.com>
2024-03-31 02:47:21 +0500

    10, 11 done

---
+++ 11_Names_Store_Arr/11_ns.js
@@ -0,0 +1,23 @@
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+var names = ['Waqar', 'Abdur Raziq', 'Jidran', 'Kashif'];
+console.log('naive method:\n');
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log('\n');
+// with forEach method
+console.log('With foreach:\n');
+names.forEach(function (name) {
+    console.log(name);
+});
+console.log('\n');
+// with for loop 
+console.log('With For Loop:\n');
+for (var ii = 0; ii < names.length; ii++) {
+    console.log(names[ii]);
+}
+console.log('\n');
+// with map, this returns null but I'm not interested in the return value in this example
+console.log('With map method:\n');
+names.map(function (name) { return console.log(name); });
---
+++ 11_Names_Store_Arr/11_ns.ts
@@ -0,0 +1,30 @@
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+const names : string[] = ['Waqar', 'Abdur Raziq', 'Jidran', 'Kashif'];
+
+console.log('naive method:\n'); 
+console.log(names[0]);
+console.log(names[1]);  
+console.log(names[2]);  
+console.log(names[3]);  
+console.log('\n');
+
+// with forEach method
+console.log('With foreach:\n')
+names.forEach(name => {
+    console.log(name); 
+});
+console.log('\n');
+
+
+// with for loop 
+console.log('With For Loop:\n'); 
+for (let ii = 0; ii < names.length; ii++){
+    console.log(names[ii]); 
+}
+console.log('\n');
+
+
+// with map, this returns null but I'm not interested in the return value in this example
+console.log('With map method:\n');
+names.map(name => console.log(name)); 
