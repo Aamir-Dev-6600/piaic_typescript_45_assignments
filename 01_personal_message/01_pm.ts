8e90253677d29bf223015cc817b890fd19af9d11
Aamir Khan Maarofi <kaamir6600@gmail.com>
2024-03-30 22:23:44 +0500

    01 Personal Message, done

--- 01_personal_message/01_pm.ts
+++ 01_personal_message/01_pm.ts
@@ -1,2 +1,10 @@
+/**
+ * Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+ */
+
+// Solution 01 - I prefer this one, always like to code this way
 const person : string = "Aamir"; 
 console.log(`Hey ${person}! How long will it take you to complete all 45 assignments?`); 
+
+// Solution 02 - naive and I hate concatinating strings in js with '+'
+console.log('Hey ' + person + '! How long will it take you to complete all 45 assignments?'); 
