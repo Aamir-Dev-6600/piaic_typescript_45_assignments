a20b740ca804dd8f195b1e81c832534e4658b433
Aamir Khan Maarofi <kaamir6600@gmail.com>
2024-03-30 22:21:16 +0500

    01 Personal Message, done

---
+++ 01_personal_message/01_pm.js
@@ -0,0 +1,2 @@
+var person = "Aamir";
+console.log("Hey ".concat(person, "! How long will it take you to complete all 45 assignments?"));
---
+++ 01_personal_message/01_pm.ts
@@ -0,0 +1,2 @@
+const person : string = "Aamir"; 
+console.log(`Hey ${person}! How long will it take you to complete all 45 assignments?`); 
