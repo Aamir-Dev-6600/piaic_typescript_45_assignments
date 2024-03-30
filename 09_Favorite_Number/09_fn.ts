41825b403edf7ad6dec585855a80cf5928079c36
Aamir Khan Maarofi <kaamir6600@gmail.com>
2024-03-31 02:34:38 +0500

    09 Favorite Number done

---
+++ 09_Favorite_Number/09_fn.ts
@@ -0,0 +1,4 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+const favoriteNumber : number = 44; 
+console.log(`Favorite Number: ${favoriteNumber}`); 
