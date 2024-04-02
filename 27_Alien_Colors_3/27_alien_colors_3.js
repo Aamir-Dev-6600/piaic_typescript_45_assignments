ec434d8482a6b733bb33bb33af13e0b50a3edf5a
Aamir Khan Maarofi <kaamir6600@gmail.com>
2024-04-03 03:58:39 +0500

    25-27 done

---
+++ 27_Alien_Colors_3/27_alien_colors_3.js
@@ -0,0 +1,65 @@
+"use strict";
+/**
+ * Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+ * • If the alien is green, print a message that the player earned 5 points.
+ * • If the alien is yellow, print a message that the player earned 10 points.
+ * • If the alien is red, print a message that the player earned 15 points.
+ * • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+// prints 5 points
+var alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("alien_color: ".concat(alien_color, " : You earned 5 points for shooting the alien"));
+}
+else if (alien_color === 'yellow') {
+    console.log("alien_color: ".concat(alien_color, " : You earned 10 points for shooting the alien"));
+}
+else if (alien_color === 'red') {
+    console.log("alien_color: ".concat(alien_color, " : You earned 15 points for shooting the alien"));
+}
+else {
+    console.log('No points earned, invalid alien color');
+}
+// prints 10 points 
+alien_color = 'yellow';
+if (alien_color === 'green') {
+    console.log("alien_color: ".concat(alien_color, " : You earned 5 points for shooting the alien"));
+}
+else if (alien_color === 'yellow') {
+    console.log("alien_color: ".concat(alien_color, " : You earned 10 points for shooting the alien"));
+}
+else if (alien_color === 'red') {
+    console.log("alien_color: ".concat(alien_color, " : You earned 15 points for shooting the alien"));
+}
+else {
+    console.log('No points earned, invalid alien color');
+}
+// prints 15 points 
+alien_color = 'red';
+if (alien_color === 'green') {
+    console.log("alien_color: ".concat(alien_color, " : You earned 5 points for shooting the alien"));
+}
+else if (alien_color === 'yellow') {
+    console.log("alien_color: ".concat(alien_color, " : You earned 10 points for shooting the alien"));
+}
+else if (alien_color === 'red') {
+    console.log("alien_color: ".concat(alien_color, " : You earned 15 points for shooting the alien"));
+}
+else {
+    console.log('No points earned, invalid alien color');
+}
+// prints (no pionts earned)
+alien_color = 'black';
+if (alien_color === 'green') {
+    console.log("alien_color: ".concat(alien_color, " : You earned 5 points for shooting the alien"));
+}
+else if (alien_color === 'yellow') {
+    console.log("alien_color: ".concat(alien_color, " : You earned 10 points for shooting the alien"));
+}
+else if (alien_color === 'red') {
+    console.log("alien_color: ".concat(alien_color, " : You earned 15 points for shooting the alien"));
+}
+else {
+    console.log('No points earned, invalid alien color');
+}
