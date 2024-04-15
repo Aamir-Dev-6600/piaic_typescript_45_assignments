59e7eaa03ce982f082e7d583338d3c691288d51b
Aamir Khan Maarofi <32407958+Aamir-Khan-Maarofi@users.noreply.github.com>
2024-04-15 13:12:22 +0500

    heptic feedback in number guessing game, and default difficulty lever

--- 02-number-guessing-game/index.ts
+++ 02-number-guessing-game/index.ts
@@ -1,65 +1,61 @@
 #!/usr/bin/env node
+
 import inquirer from "inquirer";
 
-// Computer guesses a number
-// User selects number of iterations
-// User provides number until the end is reached or the number is guessed.
-
 const numGenerated = Math.floor(Math.random() * 100);
-const maxChances = 8; 
+const maxChances = 8;
 
 console.log("\n\n\t\t**** Welcome To Guess The Number *****\n\n");
-console.log(
-  "I have a number between 01-100 (Inclusive). Can you guess? Let's paly!\n\n"
-);
+console.log("I have a number between 01-100 (Inclusive). Can you guess? Let's paly!");
+console.log(`Fun Part: You can decide the level of difficulty by choosing the number \nof choices you have, default number of chances are ${maxChances}.\n\n`);
+
 
 const inputIterations = await inquirer.prompt([
   {
-    message: `How many chances would you like to have (no more than ${maxChances}): `,
+    message: `How many chances would you like to have? (no more than ${maxChances}): `,
     type: "number",
     name: "iterations",
   },
 ]);
 
+// choose default number of iterations if the user ignores to provide the iteration count
 let iterations = !Number.isNaN(inputIterations.iterations)
   ? inputIterations.iterations
-  : 0;
-const isIterCountValid = iterations > 0 ? iterations <= maxChances : false;
+  : maxChances;
 
-if (!isIterCountValid) {
-  console.log("\n\nInvalid number of iterations, aborted.");
-} else {
-  let counter = 1;
-  while (counter <= iterations) {
-    const inputGuessed = await inquirer.prompt([
-      {
-        message: `What's Your Guess? (Chance ${counter}/${iterations}): `,
-        type: "number",
-        name: "user_number",
-      },
-    ]);
+console.log(`\nYou have ${iterations} chances to make the guess. Let's have FUN!`);
+console.log("If you want to QUIT the game, type 'Q' or 'q'\n\n");
 
-    const guessed = inputGuessed.user_number;
-    
-    if (Number.isNaN(guessed) || guessed < 1 || guessed > 100) {
-      console.log(
-        `Rejected Input! Please always input a number in range 01-100 (Both Inclusive).`
-      );
-      continue;
-    }
-    else if (guessed === numGenerated) {
-      console.log(
-        `Congratulations! You guessed correctly in ${counter} chances.`
-      );
-      break;
-    } else {
-      const message = counter == iterations ? `You Lost! Let's play again` : guessed > numGenerated ? `Unhn! That's higher than my number`: `Unhn! That's less than my number` ;
-      console.log(message);
-      counter++;
-    }
+// iterate until all the chances are exhausted
+let counter = 1;
+while (counter <= iterations) {
+  const inputGuessed = await inquirer.prompt([
+    {
+      message: `What's Your Guess? (Chance ${counter}/${iterations}): `,
+      type: "number",
+      name: "user_number",
+    },
+  ]);
+
+  const guessed = inputGuessed.user_number;
+
+  // if guessed is NaN or outside the required range, do not wate the chance
+  if (Number.isNaN(guessed) || guessed < 1 || guessed > 100) {
+    console.log(
+      `Rejected Input! Please always input a number in range 01-100 (Both Inclusive).\n`
+    );
+    continue;
   }
-
-  if (counter > iterations) {
-    console.log();
+  // break the loop, no more guesses when guessed correctly
+  else if (guessed === numGenerated) {
+    console.log(
+      `Congratulations! You guessed correctly in ${counter} chances.\n`
+    );
+    break;
+    // in all other cases, provide heptic feedback to help the guess better in next chance, or 
+  } else {
+    const message = counter == iterations ? `Gosh!!! You Lost. Want to play again?` : guessed > numGenerated ? `Unhn! That's higher than my number` : `Unhn! That's less than my number`;
+    console.log(`${message}\n`);
+    counter++;
   }
 }
